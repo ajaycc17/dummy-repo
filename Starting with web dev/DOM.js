5a5cc524f71f9a69917cb5944b1eb91756cfe64f
Ajay Choudhury <ajaycc17@gmail.com>
2023-06-20 15:12:20 +0530

    GetElementsbyTAgName

--- Starting with web dev/DOM.js
+++ Starting with web dev/DOM.js
@@ -15,10 +15,20 @@ console.log(add_items);
 add_items[0].style.fontWeight = "bold";
 add_items[0].style.color = "green";
 
-// Make the 3rd element in the list have green background color
+// Make the 3rd element in the list have green background color -- by classname
 var items = document.getElementsByClassName("list-group-item");
 items[2].style.backgroundColor = "green";
+
+// all list items have bold font
 for (let i = 0; i < items.length; i++) {
     items[i].style.fontWeight = "bold";
     items[i].style.color = "blue";
 }
+
+// Add a new li element without the same class Name
+// And try editing it with getelementsbyclassname and then by getelementbytagname
+var li = document.getElementsByTagName("li");
+// console.log(li);
+li[4].style.fontWeight = "bold";
+li[4].style.color = "red";
+li[4].style.backgroundColor = "purple";
