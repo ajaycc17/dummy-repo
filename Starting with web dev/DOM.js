3e7929ff343d919e93c0d541b5aec89d4bfacdd3
Ajay Choudhury <ajaycc17@gmail.com>
2023-06-20 15:30:18 +0530

    QuerySelector && QuerySelectorALL

--- Starting with web dev/DOM.js
+++ Starting with web dev/DOM.js
@@ -17,18 +17,39 @@ add_items[0].style.color = "green";
 
 // Make the 3rd element in the list have green background color -- by classname
 var items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
 // all list items have bold font
-for (let i = 0; i < items.length; i++) {
-    items[i].style.fontWeight = "bold";
-    items[i].style.color = "blue";
-}
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = "bold";
+//     items[i].style.color = "blue";
+// }
 
 // Add a new li element without the same class Name
 // And try editing it with getelementsbyclassname and then by getelementbytagname
 var li = document.getElementsByTagName("li");
 // console.log(li);
-li[4].style.fontWeight = "bold";
-li[4].style.color = "red";
-li[4].style.backgroundColor = "purple";
+// li[4].style.fontWeight = "bold";
+// li[4].style.color = "red";
+// li[4].style.backgroundColor = "purple";
+li[4].classList.add("list-group-item");
+
+// queryselector
+// Make the 2nd item have green background color
+var secondItem = document.querySelector(".list-group-item:nth-child(2)");
+secondItem.style.backgroundColor = "green";
+
+// Make the 3rd item invisible
+var thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.display = "none";
+
+// queryselectorall
+// Using QuerySelectorALL change the font color to green for 2nd item in the item list
+var second = document.querySelectorAll("li");
+second[1].style.color = "green";
+
+// Choose all the odd elements and make their background green using QuerySelectorALL
+var oddElements = document.querySelectorAll("li:nth-child(odd)");
+for (var i = 0; i < oddElements.length; i++) {
+    oddElements[i].style.backgroundColor = "green";
+}
