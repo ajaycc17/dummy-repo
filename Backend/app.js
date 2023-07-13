37a6086077b792537db8eeb56085731379c7f230
Ajay Choudhury <ajaycc17@gmail.com>
2023-07-13 20:10:34 +0530

    Add routes to node app

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,35 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("Ajay Choudhury");
+    console.log(req.headers, req.url, req.method);
+    // process.exit();
+    // res.setHeader("Content-Type", "text/html");
+    const url = req.url;
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>My First Page</title></head>");
+        res.write("<body><h1>Hello World</h1></body>");
+        res.write("</head>");
+        return res.end();
+    } else if (url === "/home") {
+        res.write("<html>");
+        res.write("<head><title>My First Page</title></head>");
+        res.write("<body><h1>Welcome home</h1></body>");
+        res.write("</head>");
+        return res.end();
+    } else if (url === "/about") {
+        res.write("<html>");
+        res.write("<head><title>My First Page</title></head>");
+        res.write("<body><h1>Welcome to About Us page</h1></body>");
+        res.write("</head>");
+        return res.end();
+    } else if (url === "/node") {
+        res.write("<html>");
+        res.write("<head><title>My First Page</title></head>");
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+        res.write("</head>");
+        return res.end();
+    }
 });
 
 server.listen(4000);
