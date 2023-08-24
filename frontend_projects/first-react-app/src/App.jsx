6c9a93755f70a0e8ff3a0915a865c8bafa86ab9c
Ajay Choudhury <ajaycc17@gmail.com>
2023-08-24 15:14:49 +0530

    first-react-app

---
+++ frontend_projects/first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello World!</h1>
+                <ExpenseItem />
+            </header>
+        </div>
+    );
+}
+
+export default App;
